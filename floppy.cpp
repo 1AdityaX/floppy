c98f4bda02bb9fa9dc522172bffc576ca1405d17
Sadhana <shreekutta0311@gmail.com>
2025-11-18 22:10:26 +0530

    implemented Pipe class with drawing functionality

--- floppy.cpp
+++ floppy.cpp
@@ -3,11 +3,14 @@
 
 using namespace std;
 
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 450;
-const int GRAVITY = 600;
-const int JUMP_STRENGTH = -350;
-
+const int ScreenWidth = 800;
+const int ScreenHeight = 450;
+const int Gravity = 600;
+const int JumpStrength = -350;
+const int PipeSpeed = 200;
+const int PipeSpawnX = 300; // Distance between pipes
+const int PipeWidth = 80;
+const int PipeGap = 170;
 
 class Bird
 {
@@ -23,42 +26,42 @@ public:
         radius = r;
         color = c;
         velocity = 0;
-        position = {100, SCREEN_HEIGHT / 2.0f};
+        position = {100, ScreenHeight / 2.0f};
     }
 
-    Vector2 GetPosition() 
-    { 
-        return position; 
+    Vector2 GetPosition()
+    {
+        return position;
     }
 
-    float GetVelocity() 
-    { 
-        return velocity; 
+    float GetVelocity()
+    {
+        return velocity;
     }
 
     float GetRadius()
-    { 
-        return radius; 
+    {
+        return radius;
     }
 
-    void SetPosition(Vector2 pos) 
-    { 
-        position = pos; 
+    void SetPosition(Vector2 pos)
+    {
+        position = pos;
     }
 
-    void SetVelocity(float vel) 
-    { 
-        velocity = vel; 
+    void SetVelocity(float vel)
+    {
+        velocity = vel;
     }
 
     void Jump()
     {
-        velocity = JUMP_STRENGTH;
+        velocity = JumpStrength;
     }
 
     void Update(float dt)
     {
-        velocity += GRAVITY * dt;
+        velocity += Gravity * dt;
         position.y += velocity * dt;
     }
 
@@ -75,6 +78,74 @@ public:
     }
 };
 
-int main() {
+class Pipe
+{
+private:
+    float x;
+    float gapY; // The center Y position of the gap
+    bool passed;
+
+public:
+    Pipe(float xPos = ScreenWidth, float gap = ScreenHeight / 2.0f)
+    {
+        x = xPos;
+        gapY = gap;
+        passed = false;
+    }
+
+    float GetX()
+    {
+        return x;
+    }
+
+    float GetGapY()
+    {
+        return gapY;
+    }
+
+    bool IsPassed()
+    {
+        return passed;
+    }
+
+    void SetX(float xPos)
+    {
+        x = xPos;
+    }
+    void SetGapY(float gap)
+    {
+        gapY = gap;
+    }
+    void SetPassed(bool p)
+    {
+        passed = p;
+    }
+
+    void Update(float dt)
+    {
+        x -= PipeSpeed * dt;
+    }
+
+    void Draw() const
+    {
+        // Top Pipe
+        Rectangle topRec = {x, 0, (float)PipeWidth, gapY - (PipeGap / 2)};
+        DrawRectangleRec(topRec, GREEN);
+        DrawRectangleLinesEx(topRec, 3, DARKGREEN); // Outline
+
+        // Bottom Pipe
+        Rectangle bottomRec = {x, gapY + (PipeGap / 2), (float)PipeWidth, (float)ScreenHeight - (gapY + (PipeGap / 2))};
+        DrawRectangleRec(bottomRec, GREEN);
+        DrawRectangleLinesEx(bottomRec, 3, DARKGREEN); // Outline
+    }
+};
+
+int main()
+{
+    InitWindow(ScreenWidth, ScreenHeight, "Raylib - Flappy Bird Clone");
+    SetTargetFPS(60);
+    srand(time(NULL));
+
+    Bird bird(20, GOLD);
     return 0;
 }
